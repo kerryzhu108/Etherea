058f73f8531691d643d021c7eff6be25bd8695ae
Morgan Chang <morganchang@Morgans-MacBook-Pro.local>
2021-10-23 15:20:39 -0400

    test db connection

--- backend/src/routes/root.js
+++ backend/src/routes/root.js
@@ -7,4 +7,25 @@ router.get("/",
         return res.json({ "message": "Message received!" });
     });
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+router.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
 module.exports = router;
