89f91a763847b09c42298f1501be490e12d26927
Morgan Chang <morganchang@Morgans-MacBook-Pro.local>
2021-10-23 15:25:38 -0400

    test db connection

--- backend/src/routes/root.js
+++ backend/src/routes/root.js
@@ -20,7 +20,8 @@ router.get('/db', async (req, res) => {
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM users');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
+      //res.render('pages/db', results );
+      res.json(results);
       client.release();
     } catch (err) {
       console.error(err);
