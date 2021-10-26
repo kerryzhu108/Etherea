3dac4cef3324ab41e6d4a19810c07f4d228c2fb6
Morgan Chang <morganchang@Morgans-MacBook-Pro.local>
2021-10-25 20:12:10 -0400

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 
+// app.set('views', 'src/views');
+// app.set('view engine', 'ejs');
+
 // Route endpoints
 app.use("/", rootRouter);
 
