0af14dc48cc4b849f688dc10584562f98dcfe5c2
Morgan Chang <morganchang@Morgans-MacBook-Pro.local>
2021-10-27 17:10:37 -0400

    set up db in dev or produc mode

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,14 +1,27 @@
-const config = {
-    db: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    }
-};
+const { Pool } = require('pg');
 
-module.exports = config;
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: isProduction
+  });
+
+// const config = {
+//     db: {
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PASS,
+//         database: process.env.DB_NAME,
+//         ssl: {
+//             rejectUnauthorized: false
+//         }
+//     }
+// };
+
+// module.exports = config;
+
+module.exports = { pool };
--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -2,38 +2,64 @@ const express = require('express');
 const { Pool } = require('pg');
 var router = express.Router();
 const { check, validationResult } = require('express-validator');
-const config = require('../config');
 const bcrypt = require('bcrypt');
 const { generateAccessToken, authenticateToken } = require("../token");
 
-const pool = new Pool(config.db);
+const { pool } = require('../config');
+//const config = require('../config');
+//const pool = new Pool(config.db);
 
 // Registration endpoint
 router.post('/register', [
     check('email').isEmail(),
     check("first_name").isString(),
     check("last_name").isString(),
-    check("password").isString()
+    check("password").isString(),
+    check("password2").isString()
 ], async (req, res) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+
+    if (password.length < 8) {
+        errors.push({ message: "Password must be a least 8 characters long" });
+    }
+    
+    if (password !== password2) {
+        errors.push({ message: "Passwords do not match" });
     }
 
-    const body = req.body;
+    if (!errors.isEmpty()) {
+        // rerender the sign up page showing the error messages?
+        // res.render("signUp.js", { errors });
+        return res.status(400).json({ errors: errors.array() });
+    } else {
+        const body = req.body;
 
-    const hashed_password = await bcrypt.hash(body.password, 10);
+        const hashed_password = await bcrypt.hash(body.password, 10);
 
-    // Attempt to insert the user into the database
-    pool.query("INSERT INTO USERS (email, password, firstname, lastname) values ($1, $2, $3, $4)",
-        [body.email, hashed_password, body.first_name, body.last_name], (err, result) => {
-            if (err) {
+        pool.query("SELECT * FROM USERS WHERE email = $1", 
+            [body.email], (err, results) => {
+              if (err) {
                 return res.status(400).json({ error: { message: err.toString() } });
+              }      
+              if (results.rows.length > 0) {
+                //errors.push({ message: "Email already registered" })
+                //return res.render("signUp.js", { message: "Email already registered" });
+                return res.status(400).json({ error: { message: "Email already registered" } });
+              } 
             }
+        );
 
-            // On successful registration, return successful response
-            return res.json({ message: `Successfully created user` });
-        });
+        // Attempt to insert the user into the database
+        pool.query("INSERT INTO USERS (email, password, firstname, lastname) values ($1, $2, $3, $4)",
+            [body.email, hashed_password, body.first_name, body.last_name], (err, result) => {
+                if (err) {
+                    return res.status(400).json({ error: { message: err.toString() } });
+                }
+
+                // On successful registration, return successful response
+                return res.json({ message: `Successfully created user` });
+            });
+    }
 });
 
 router.post('/login', [
