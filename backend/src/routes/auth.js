ec4056c5a88d24c484341ed01925e22f95225579
Zachary Vincze <zacharyvincze@gmai.com>
2021-10-29 00:39:50 -0400

    Fixed auth.js

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -13,60 +13,51 @@ router.post('/register', [
     check("email").isEmail(),
     check("first_name").isString(),
     check("last_name").isString(),
-    check("password").isString(),
-    check("password2").isString()
+    check("password").isString().isLength({ min: 8 })
 ], async (req, res) => {
     const errors = validationResult(req);
-
-    if (password.length < 8) {
-        errors.push({ message: "Password must be a least 8 characters long" });
-    }
-    
-    if (password !== password2) {
-        errors.push({ message: "Passwords do not match" });
-    }
 
     if (!errors.isEmpty()) {
         // rerender the sign up page showing the error messages?
         // res.render("signUp.js", { errors });
         return res.status(400).json({ errors: errors.array() });
     } else {
-        const body = req.body;
 
+        const body = req.body;
         const hashed_password = await bcrypt.hash(body.password, 10);
 
-        pool.query("SELECT * FROM USERS WHERE email = $1", 
+        pool.query("SELECT * FROM USERS WHERE email = $1",
             [body.email], (err, results) => {
-              if (err) {
-                return res.status(400).json({ error: { message: err.toString() } });
-              }      
-              if (results.rows.length > 0) {
-                //errors.push({ message: "Email already registered" })
-                //return res.render("signUp.js", { message: "Email already registered" });
-                return res.status(400).json({ error: { message: "Email already registered" } });
-              } 
-            }
-        );
-
-        // Attempt to insert the user into the database
-        pool.query("INSERT INTO Users (email, password, firstname, lastname) values ($1, $2, $3, $4) RETURNING id",
-            [body.email, hashed_password, body.first_name, body.last_name], (err, result) => {
                 if (err) {
                     return res.status(400).json({ error: { message: err.toString() } });
                 }
+                if (results.rows.length > 0) {
+                    //errors.push({ message: "Email already registered" })
+                    //return res.render("signUp.js", { message: "Email already registered" });
+                    return res.status(400).json({ error: { message: "Email already registered" } });
+                }
 
-                // On successful registration, add user id to table progressInfo
-                var userId = result.rows[0].id;
-                pool.query("INSERT INTO progressInfo (id) VALUES ($1)", [userId], 
-                    (err, result) => {
+                // Attempt to insert the user into the database
+                pool.query("INSERT INTO Users (email, password, firstname, lastname) values ($1, $2, $3, $4) RETURNING id",
+                    [body.email, hashed_password, body.first_name, body.last_name], (err, result) => {
                         if (err) {
                             return res.status(400).json({ error: { message: err.toString() } });
                         }
-                    });
 
-                // On successful registration, return successful response
-                return res.json({ message: `Successfully created user` });
-            });
+                        // On successful registration, add user id to table progressInfo
+                        var userId = result.rows[0].id;
+                        pool.query("INSERT INTO progressInfo (id) VALUES ($1)", [userId],
+                            (err, result) => {
+                                if (err) {
+                                    return res.status(400).json({ error: { message: err.toString() } });
+                                }
+                            });
+
+                        // On successful registration, return successful response
+                        return res.json({ message: `Successfully created user` });
+                    });
+            }
+        );
     }
 });
 
@@ -87,6 +78,10 @@ router.post('/login', [
                 return res.status(400).json({ error: { message: err.toString() } });
             }
 
+            if (result.rows.length <= 0) {
+                return res.status(401).json({ error: { message: "Email does not exist" } });
+            }
+
             // On successful registration, return successful response
             const password = result.rows[0].password;
             bcrypt.compare(body.password, password, (err, isMatch) => {
@@ -98,7 +93,7 @@ router.post('/login', [
                     // Generate an access token and send back to client
                     return res.json({ tokens: { access: generateAccessToken(body.email) } });
                 } else {
-                    return res.status(401).json({ error: { message: "Could not log in user, incorrect credentials." } });
+                    return res.status(401).json({ error: { message: "Invalid password" } });
                 }
             });
         });
