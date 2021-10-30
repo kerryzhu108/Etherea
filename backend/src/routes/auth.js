366919f3afc45d137a283634acef02d473ac7b2b
Morgan Chang <morganchang@Morgans-MacBook-Pro.local>
2021-10-30 05:04:54 -0400

    fix db insert issue

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -20,22 +20,25 @@ router.post('/register', [
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        //return res.json(req.body);
         return res.status(400).json({ errors: errors.array() });
     } else {
 
         const body = req.body;
         const hashed_password = await bcrypt.hash(body.password, 10);
 
-        pool.query("SELECT * FROM USERS WHERE email = $1",
-            [body.email], (err, results) => {
+        pool.query("SELECT * FROM USERS WHERE email = $1", [body.email], 
+            (err, results) => {
                 if (err) {
                     return res.status(400).json({ error: { message: err.toString() } });
                 }
                 if (results.rows.length > 0) {
                     return res.status(400).json({ error: { message: "Email already registered" } });
                 }
+            }
+        );
 
+        // Attempt to insert the user into the database
+        // On successful registration, add user id to table progressInfo and table impactStats
         ;(async () => {
             const client = await pool.connect()
             try {
@@ -48,38 +51,18 @@ router.post('/register', [
             } finally {
                 client.release()
             }
-            })().catch(err => { return res.status(400).json( {error: { message: err.toString() } } ) } )
+            })()
+            .catch(
+                err => { 
+                    return res.status(400).json( { error: { message: err.toString() } } ) 
+                }
+            )
 
-                // Attempt to insert the user into the database
-                // pool.query("INSERT INTO Users (email, password, firstname, lastname) values ($1, $2, $3, $4) RETURNING uid",
-                //     [body.email, hashed_password, body.first_name, body.last_name], (err, result) => {
-                //         if (err) {
-                //             return res.status(400).json({ error: { message: err.toString() } });
-                //         }
+        // On successful registration, return successful response
+        return res.json({ message: `Successfully created user` });
 
-                //         // On successful registration, add user id to table progressInfo and table impactStats;
-                //         var userId = result.rows[0].uid;
-                //         pool.query("INSERT INTO progressInfo (id) VALUES ($1)", [userId],
-                //             (err, result) => {
-                //                 if (err) {
-                //                     console.log("error inserting into progressInfo");
-                //                     return res.status(400).json({ error: { message: err.toString() } });
-                //                 }
-                //             });
-                //         pool.query("INSERT INTO impactStats (uid) VALUES ($1)", [userId],
-                //             (err, result) => {
-                //                 if (err) {
-                //                     console.log("error inserting into impactStats");
-                //                     return res.status(400).json({ error: { message: err.toString() } });
-                //                 }
-                //             });
+    }
 
-                        // On successful registration, return successful response
-                        return res.json({ message: `Successfully created user` });
-                    });
-            }
-        //);
-    //}
 });
 
 router.post('/login', [
