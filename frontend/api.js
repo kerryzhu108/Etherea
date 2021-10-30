9cdca4a9b69cf7bf40fa45dfae947bef657d494e
Morgan Chang <morganchang@Morgans-MacBook-Pro.local>
2021-10-30 05:45:05 -0400

    set up login api

--- frontend/api.js
+++ frontend/api.js
@@ -25,6 +25,23 @@ export function signUp(firstName, lastName, email, password, confirmPassword) {
   })
 }
 
+export function login(email, password) {
+  fetch(domain + 'auth/login', {
+   method: 'POST',
+   headers: defaultHeaders,
+   mode: 'cors',
+   cache: 'default',
+   body: JSON.stringify({
+     "email": email,
+     "password": password
+   })
+ }).then((response) => response.json())
+ .then((responseData) => {
+   console.log(responseData);
+   return responseData;
+ })
+}
+
 export function getTotalCost(checkoutItems, discountCode) {
   return fetch(domain + 'get_total_cost', {
     method: 'POST',
