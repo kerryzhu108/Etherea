d92a5ff98e606a03950153f3beccb893bfa04174
Zachary Vincze <zacharyvincze@gmai.com>
2021-11-01 11:47:24 -0400

    Added username to home screen

--- frontend/apis/headers.js
+++ frontend/apis/headers.js
@@ -1,6 +1,6 @@
 'use strict'
 
-export const domain = 'http://192.168.1.3:3000/' //https://etherea-dev.herokuapp.com/'
+export const domain = 'http://192.168.2.30:3000/' //https://etherea-dev.herokuapp.com/'
 
 export const defaultHeaders = {
   'Accept': 'application/json',
