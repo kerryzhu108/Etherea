42f3d3a4d646c287cb04a8aa871c1e113d6a64f1
Andy Chan <andykaysum.chan@mail.utoronto.ca>
2021-11-01 18:10:00 -0400

    updated impact stats calc

--- backend/src/routes/impactStats.js
+++ backend/src/routes/impactStats.js
@@ -7,7 +7,8 @@ const { pool } = require('../config');
 router.get("/impactStats/:userid", async (req, res) => {
     const { userid } = req.params
     try {
-        const result = await pool.query("SELECT * FROM ImpactStats WHERE uid = $1;", [userid]);
+        //const result = await pool.query("SELECT * FROM ImpactStats WHERE uid = $1;", [userid]);
+        const result = await pool.query("SELECT id, 100*exp as emissionsreduced, exp/5 as animalssaved FROM progressinfo WHERE id = $1;", [userid]);
         res.json(result.rows);
     } catch (error) {
         return res.status(500).json({ error: { message: error.toString() } });
