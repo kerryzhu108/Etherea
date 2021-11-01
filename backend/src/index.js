9fb9b949ae5bea39a9756a90e14ed73b7797f211
Zachary Vincze <zacharyvincze@gmai.com>
2021-11-01 17:19:51 -0400

    Removed IP from app listen

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,6 @@ const app = require("./app");
 const port = 3000;
 
 // Application start
-app.listen(process.env.PORT || port, process.env.IP || "localhost", () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`Server running on port ${process.env.PORT}`);
 });
