bb8eeff17b4ce40db9c3e9cdf3c0db7b08bf1ef0
Zachary Vincze <zacharyvincze@gmai.com>
2021-11-01 02:08:12 -0400

    Standardized impact stats error

--- backend/src/routes/impactStats.js
+++ backend/src/routes/impactStats.js
@@ -7,11 +7,10 @@ const { pool } = require('../config');
 router.get("/impactStats/:userid", async(req, res) =>{
     const { userid } = req.params
     try {
-        //NOTE: currently table does not exist on db
-        const info = await pool.query("SELECT * FROM ImpactStats WHERE uid = $1;", [userid]);
+        await pool.query("SELECT * FROM ImpactStats WHERE uid = $1;", [userid]);
         res.json(info.rows)
     } catch (error) {
-        return res.status(400).send(error.message);
+        return res.status(500).json({error: {message: error.toString()}});
     }
     finally{
         res.end();
