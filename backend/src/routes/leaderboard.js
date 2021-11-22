bec05eaaf887a921d326c24043ad14258f87569e
Andy Chan <andykaysum.chan@mail.utoronto.ca>
2021-11-22 17:05:22 -0500

    added level information in leaderboard get request

--- backend/src/routes/leaderboard.js
+++ backend/src/routes/leaderboard.js
@@ -4,12 +4,14 @@ var router = express.Router();
 
 const { pool } = require('../config');
 
+var level_exp = 50//experience required for each level
+
 router.get('/leaderboard', async (req, res) => {
     try {
       const client = await pool.connect();
       //naive approach: send entire leaderboard worth of data, may need to change to paginate
       const result = await client.query(
-          "SELECT u.uid, CONCAT(u.firstname, ' ', u.lastname) AS name, p.exp FROM users u, progressInfo p WHERE u.uid = p.id ORDER BY p.exp DESC;");
+          "SELECT u.uid, CONCAT(u.firstname, ' ', u.lastname) AS name, p.exp, p.exp/$1 AS level FROM users u, progressInfo p WHERE u.uid = p.id ORDER BY p.exp DESC;", [level_exp]);
       const results = { 'results': (result) ? result.rows : null};
       
       res.json(results);
