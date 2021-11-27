f7408a4777f51bcbd0f05c197e736c6fa94461c5
shin19991207@gmail.com <shin19991207@gmail.com>
2021-11-27 15:51:02 -0500

    arrange text position

--- frontend/views/landing.js
+++ frontend/views/landing.js
@@ -11,8 +11,9 @@ export default class Landing extends React.Component {
         <ImageBackground source={require('../assets/landingPage.png')}
           resizeMode="cover" style={styles.background}>
           <View style={styles.loginButtons}>
-            <Text style={styles.navigation} onPress={() => this.props.navigation.navigate('Login')}>Login</Text>
-            <Text style={styles.navigation} onPress={() => this.props.navigation.navigate('SignUp')}>Sign Up</Text>
+            <Text style={styles.login} onPress={() => this.props.navigation.navigate('Login')}>Login</Text>
+            <View style={styles.verticleLine}></View>
+            <Text style={styles.signup} onPress={() => this.props.navigation.navigate('SignUp')}>Sign Up</Text>
           </View>
         </ImageBackground>
       </View>
@@ -33,11 +34,25 @@ const styles = StyleSheet.create({
     fontSize: 30,
     marginTop: 120,
   },
-  navigation: {
+  login: {
     fontSize: 20,
-    marginTop: 480,
-    textDecorationLine: 'underline',
+    marginTop: 780,
     marginRight: 15,
+    color: '#4C494A',
+    fontWeight: '500'
+  },
+  signup: {
+    fontSize: 20,
+    marginTop: 780,
+    marginLeft: 15,
+    color: '#4C494A',
+    fontWeight: '500'
+  },
+  verticleLine: {
+    marginTop: 770,
+    height: '6%',
+    width: 2,
+    backgroundColor: '#4C494A',
   },
   loginButtons: {
     flexDirection: 'row'
