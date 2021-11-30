79132f5ce534eee226ba0bb7039ec971f4d527c8
Jayden Ong <jayden.ong@mail.utoronto.ca>
2021-11-30 17:08:08 -0500

    Modified admin controls to remove all unnecessary code

--- backend/src/routes/admincontrols.js
+++ backend/src/routes/admincontrols.js
@@ -10,16 +10,15 @@ router.post("/themesTasks/:themeID", async(req, res) =>{
     try{
         // Create task
         const {themeID, descript, taskName, points} = req.body;
-        const new_entry = req.body;
 
         // Get any rows in database that have the same task name
-        var result = await pool.query("SELECT * FROM v_theme_task WHERE taskName=$1", [new_entry.taskName])
+        var result = await pool.query("SELECT * FROM v_theme_task WHERE taskName=$1", [taskName])
         if(result.rows.length > 0){
             return res.status(500).json({ error: {message: "A task with this name already exists."}})
         }
 
         // Adding new task to database
-        await pool.query("INSERT INTO themesTasks (themeID, descript, taskName, points) VALUES ($1, $2, $3, $4) RETURNING *", [new_entry.themeID, new_entry.descript, new_entry.taskName, new_entry.points]);
+        await pool.query("INSERT INTO themesTasks (themeID, descript, taskName, points) VALUES ($1, $2, $3, $4) RETURNING *", [themeID, descript, taskName, points]);
         return res.json({message: "The new task has successfully been created."});
     } catch(error) {
         return res.status(400).send(error.message);
@@ -30,11 +29,10 @@ router.post("/themesTasks/:themeID", async(req, res) =>{
 router.post("/themesAll", async(req, res) =>{
     try{
         // Setting the id, theme, statName, multiplier, and date to be launched
-        const {id, theme, statName, multiplier, datelaunched, color} = req.body;
-        const new_entry = req.body;
+        const {theme, statName, multiplier, datelaunched, color} = req.body;
 
         // Trying to determine if any themes are already in the database
-        var result = await pool.query("SELECT * FROM themes WHERE theme=$1", [new_entry.theme])
+        var result = await pool.query("SELECT * FROM themes WHERE theme=$1", [theme])
         if(result.rows.length > 0){
             return res.status(500).json({ error: {message: "This task has already been created."}})
         }
