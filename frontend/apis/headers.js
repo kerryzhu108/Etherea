3b25b3194f894d248e39b37f8df298bf70462f73
Zachary Vincze <zacharyvincze@gmail.com>
2021-12-01 15:34:11 -0500

    Fixed progress page and related endpoints

--- frontend/apis/headers.js
+++ frontend/apis/headers.js
@@ -1,7 +1,6 @@
 'use strict'
 
 export const domain = 'https://etherea-dev.herokuapp.com/'
-// export const domain = 'http://127.0.0.1:3000/'
 
 export const defaultHeaders = {
   'Accept': 'application/json',
