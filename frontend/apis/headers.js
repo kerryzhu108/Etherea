634fe1cb0b12e666886af3b6c9dda290dccdf59b
kerryzhu08 <jiayu.zhu@mail.utoronto.ca>
2021-12-01 05:39:17 -0500

    added back headers

---
+++ frontend/apis/headers.js
@@ -0,0 +1,8 @@
+'use strict'
+
+export const domain = 'https://etherea-dev.herokuapp.com/'
+
+export const defaultHeaders = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json'
+}
