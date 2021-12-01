27118e2197662f1c9dd3266106e4b3f35e0fe2bf
shin19991207@gmail.com <shin19991207@gmail.com>
2021-12-01 10:54:50 -0500

    pull

--- frontend/apis/headers.js
+++ frontend/apis/headers.js
@@ -1,7 +1,7 @@
 'use strict'
 
 export const domain = 'https://etherea-dev.herokuapp.com/'
-// export const domain = 'http://192.168.0.114:3000/'
+// export const domain = 'http://127.0.0.1:3000/'
 
 export const defaultHeaders = {
   'Accept': 'application/json',
