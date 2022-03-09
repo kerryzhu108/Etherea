27e971575c21b5bb623003a4a991d87d75e64287
kerryzhu08 <jiayu.zhu@mail.utoronto.ca>
2022-03-08 20:17:15 -0500

    moved server to own heroku

--- backend/src/config.js
+++ backend/src/config.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg');
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const connectionString = `${process.env.DATABASE_URL}` || `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
